e5da80767bf76702c82c7436c0fa8f6e6e045530
Isaac Peñaloza <yeryreik@gmail.com>
2026-08-13 16:48:20 -0500

    cambio

--- Repaso03/src/main/java/pe/edu/upeu/repaso03/ArreglosVM.java
+++ Repaso03/src/main/java/pe/edu/upeu/repaso03/ArreglosVM.java
@@ -1,39 +1,83 @@
 package pe.edu.upeu.repaso03;
 
 public class ArreglosVM {
-    public static void meses(){
-        String[] mes={"Enero", "Febrero", "Marzo","Abril", "Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre" };
+    public static void meses() {
+        String[] mes = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
-        System.out.println("Tamaño: "+mes.length);
-        for(String m:mes) {
+        System.out.println("Tamaño: " + mes.length);
+        for (String m : mes) {
             System.out.println(m);
         }
-        mes[11]="";
+        mes[11] = "";
         System.out.println("");
-        for(String m:mes) {
-            if(m.equals(""));
+        for (String m : mes) {
+            if (m.equals("")) ;
             System.out.println(m);
 
 
         }
     }
-    public static void imprimirMatriz(int [][] m){
+
+    public static void imprimirMatriz(int[][] m) {
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[1].length; j++) {
-                if(m[i][j]!=0){
-                    System.out.print(m[i][j]+"\t");
-                }else{
+                if (m[i][j] != 0) {
+                    System.out.print(m[i][j] + "\t");
+                } else {
                     System.out.print(" \t");
                 }
             }
             System.out.println("");
         }
     }
-    
+
+    public static int[][] matrizF5(int tamaño, int numI) {
+        int matriz[][] = new int[tamaño][tamaño];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int c = matriz[0].length - 1; c >= matriz[0].length - 1 - i; c--) {
+                matriz[i][c] = numI;
+                numI++;
+            }
+        }
+        return matriz;
+
+    }
+
+
+    public static int[][] matrizF11(int tamanho, int numI) {
+        int[][] matriz = new int[tamanho][tamanho];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = matriz[0].length - 1 - f; c >= 0; c--) {
+                matriz[f][c] = numI;
+                numI++;
+            }
+        }
+        return matriz;
+    }
+    public static int[][] matrizF13(int tamanho, int numI){
+        int[][] matriz=new int[tamanho][tamanho];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c =0; c<= matriz[0].length-1-f; c++) {
+                matriz[c][f]=numI;
+                numI++;
+            }
+        }
+        return matriz;
+    }
+
+
+
     public static void main(String[] args) {
-        meses();
-        int[][] m={{1,2,6},{3,4,2},{5,6,7}};
-        imprimirMatriz(m);
+        //meses();
+        //int[][] m={{1,2,6},{3,4,2},{5,6,7}};
+        //imprimirMatriz(m);
+        imprimirMatriz(matrizF5(5,1));
+        System.out.println("");
+        imprimirMatriz(matrizF11(5,1));
+        System.out.println("");
+        imprimirMatriz(matrizF13(5,1));
+        System.out.println("");
+
 
 
     }
